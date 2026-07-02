c63067aad6f2bc6b99a01be9aa919647f68373cd
alex6712 <alexeivanyukov@yandex.ru>
2026-07-02 14:10:17 +0300

    Незначительные улучшения

--- app/core/consts.py
+++ app/core/consts.py
@@ -11,6 +11,11 @@ USERNAME_MIN_LENGTH = 3
 USERNAME_MAX_LENGTH = 32
 """Максимальная длина имени пользователя в символах."""
 
+if USERNAME_MIN_LENGTH > USERNAME_MAX_LENGTH:
+    raise ValueError(
+        f"Username min length ({USERNAME_MIN_LENGTH}) can't be larger than max length ({USERNAME_MAX_LENGTH})!"
+    )
+
 PASSWORD_MIN_LENGTH = 12
 """Минимальная длина пароля в символах."""
 
@@ -23,22 +28,27 @@ DISPLAY_NAME_MIN_LENGTH = 1
 DISPLAY_NAME_MAX_LENGTH = 32
 """Максимальная длина отображаемого имени пользователя в символах."""
 
+if DISPLAY_NAME_MIN_LENGTH > DISPLAY_NAME_MAX_LENGTH:
+    raise ValueError(
+        f"Display name min length ({DISPLAY_NAME_MIN_LENGTH}) can't be larger than max length ({DISPLAY_NAME_MAX_LENGTH})!"
+    )
+
 DEFAULT_OFFSET = 0
 """Значение смещения по умолчанию для пагинации."""
 
 MAX_OFFSET = 250
 """Максимально допустимое смещение для пагинации."""
 
+if DEFAULT_OFFSET > MAX_OFFSET:
+    raise ValueError(
+        f"Default offset value ({DEFAULT_OFFSET}) can't be larger than max value ({MAX_OFFSET})!"
+    )
+
 DEFAULT_LIMIT = 10
 """Количество элементов на странице по умолчанию."""
 
 MAX_LIMIT = 50
 """Максимально допустимое количество элементов на странице."""
-
-if DEFAULT_OFFSET > MAX_OFFSET:
-    raise ValueError(
-        f"Default offset value({DEFAULT_OFFSET}) can't be larger than max value ({MAX_OFFSET})!"
-    )
 
 if DEFAULT_LIMIT > MAX_LIMIT:
     raise ValueError(
