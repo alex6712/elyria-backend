e8928ac1d90c3fe3ddfb335366ad7c51f0c8121e
alex6712 <alexeivanyukov@yandex.ru>
2026-08-03 12:03:46 +0300

    refactor(users): порты репозиториев опущены из Application в Domain Layer
    - Перенесены IdentityRepository, SessionRepository и ProfileRepository
      в подпакет src/users/domain/repositories
    - Обновлены импорты UsersUnitOfWork и SqlAlchemyUsersUnitOfWork
    - В application/ports/persistence оставлен только TokenBlacklist
    - Решение зафиксировано ADR-0008

--- src/users/infrastructure/sqlalchemy_users_uow.py
+++ src/users/infrastructure/sqlalchemy_users_uow.py
@@ -3,7 +3,7 @@ from types import TracebackType
 from sqlalchemy.ext.asyncio import AsyncConnection, AsyncEngine, AsyncTransaction
 
 from src.shared.application.exception import UnitOfWorkNotEnteredError
-from src.users.application.ports.persistence import (
+from src.users.domain.repositories import (
     IdentityRepository,
     ProfileRepository,
     SessionRepository,
